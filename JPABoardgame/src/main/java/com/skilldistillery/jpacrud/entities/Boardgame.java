11646731e9aa241b4f7300fc0df75b9783947a71
Ashley Zink <pamperedzink@gmail.com>
2020-10-23 18:23:41 -0600

    added Junit test

--- JPABoardgame/src/main/java/com/skilldistillery/jpacrud/entities/Boardgame.java
+++ JPABoardgame/src/main/java/com/skilldistillery/jpacrud/entities/Boardgame.java
@@ -1,5 +1,47 @@
 package com.skilldistillery.jpacrud.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Boardgame {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+
+	public Boardgame() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Boardgame [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
