928064f7f5b5b84b3160c08aa983e7a5c50a0ef9
Ashley Zink <pamperedzink@gmail.com>
2020-10-24 07:15:23 -0600

    updated gradle settings and build, added controller

---
+++ BoardgameCrud/src/main/java/com/skilldistillery/jpacrud/controllers/BoardgameController.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.jpacrud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardgameController {
+	
+	@RequestMapping(path="/")
+	public String index() {
+		return "index.jsp";
+	}
+
+}
