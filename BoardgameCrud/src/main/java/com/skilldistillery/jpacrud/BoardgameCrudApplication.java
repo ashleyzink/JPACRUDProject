8dd4b28d750f821357248edf3ef48e4449717cbb
Ashley Zink <pamperedzink@gmail.com>
2020-10-26 10:13:39 -0600

    added war file

--- BoardgameCrud/src/main/java/com/skilldistillery/jpacrud/BoardgameCrudApplication.java
+++ BoardgameCrud/src/main/java/com/skilldistillery/jpacrud/BoardgameCrudApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BoardgameCrudApplication {
+public class BoardgameCrudApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoardgameCrudApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardgameCrudApplication.class, args);
